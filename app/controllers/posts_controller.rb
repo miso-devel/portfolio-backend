9ac92aaac52111506162e9083791f8f0819dd95d
nisi0929 <kengo279@icloud.com>
2022-05-04 02:15:33 +0900

    add strong patrameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :url)
   end
 end

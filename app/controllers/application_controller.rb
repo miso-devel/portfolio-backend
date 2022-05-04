945bcbfa9e982bda33b53ef5e7579736cd59cf6b
nisi0929 <kengo279@icloud.com>
2022-05-05 01:55:21 +0900

    user-auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+    def login!
+        session[:user_id] = @user.id
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
 end

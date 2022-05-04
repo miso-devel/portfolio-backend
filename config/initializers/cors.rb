954b13285c230db6a5214f219b7fd3954f74ee57
nisi0929 <kengo279@icloud.com>
2022-05-05 05:18:03 +0900

    heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000','https://done-portfolio.herokuapp.com/'
+    if Rails.env.production?
+      origins 'https://done-portfolio.herokuapp.com'
+    else
+      origins 'http://localhost:3000'
+    end
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],

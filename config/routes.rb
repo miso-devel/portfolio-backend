0da9651b69dc2b1e836dde67fe36c04bfc876c91
nisi0929 <kengo279@icloud.com>
2022-05-04 15:47:30 +0900

    auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post '/signup', to: 'registrations#signup'
+  post '/login', to: 'sessions#login'
+  delete '/logout', to: 'sessions#logout'
+  get '/logged_in', to: 'sessions#logged_in?'
   # Defines the root path route ("/")
   # root "articles#index"
 end
